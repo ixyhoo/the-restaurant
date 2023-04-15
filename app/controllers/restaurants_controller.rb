4505bdaba0f7167770a521c2f87edf2bc4e677cf
ixyhoo <ixyhoo@outlook.fr>
2023-04-15 15:12:41 +0200

    complete step one

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,11 @@
 class RestaurantsController < ApplicationController 
-
+before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 def index
     @restaurants = Restaurant.all
 end
 
 def show
-    @restaurant = Restaurant.find(params[:id])
+
 end
 
 def new
@@ -19,21 +19,24 @@ def create
 end
 
 def edit
-    @restaurant = Restaurant.find(params[:id])
+    
 end
 
 def update
-    @restaurant = restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
     redirect_to restaurant_path(@restaurant)
 end
+
 def destroy
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
     redirect_to restaurant_path, status: :see_other
 end
 
 private
+
+def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+end
 
 def restaurant_params
     params.require(:restaurant).permit(:name, :address; :rating)
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "restaurants", to: "restaurants#index"
-  get "restaurants/:id", to: "restaurants#show", as: :restaurant
-  get "restaurants/new", to: "restaurants#new"
-  get "restaurants", to: "restaurants#create"
-  get "restaurants/:id/edit", to: "restaurant#edit"
-  patch "restaurants/:id", to: "restuarants#update"
-  delete "restaurant/:id", to: "restaurant#destroy"
+ rsources :restaurants, only: [:create, :index, :destroy]
 end
